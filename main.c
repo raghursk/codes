9e3159382bf1b710e14dd7adbf7b96d9d69fa38e
raghursk <108519569+raghursk@users.noreply.github.com>
2024-03-19 12:48:18 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+// int main ()
+// {
+//   int pldm = 1;	//lets assum
+//   printf ("Enter the String:\n");
+//   char *string = (char *) malloc (100 * sizeof (char));
+//   scanf ("%[^\n]", string);
+  
+//   int i, length = strlen (string);
+//   printf("%d\n",length);
+//   for (i = 0; i < length / 2; i++)
+// 	{
+// 	  if (string[i] != string[length - i - 1]);
+// 	  {
+// 		pldm = 0;
+// 		break;
+// // 		int temp = string[i];
+// // 		string[i] = string[length - i - 1];
+// // 		string[length - i - 1] = temp;
+// 	  }
+// 	}
+	
+//   if (pldm)
+// 	{
+// 	  printf ("enterd string is a pleandrom %s ", string);
+// 	}
+//   else
+// 	{
+//     printf("enterd string is not a pleandrom %s ",string);
+// 	}
+// }
