1b43f11e2fa5231212f7ce28d857d0953ec083e6
raghursk <108519569+raghursk@users.noreply.github.com>
2024-03-20 20:13:37 +0530

    yyyy

--- main (7).c
+++ main (7).c
@@ -1,18 +1,27 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    printf("enter the numbers:\n");
-    scanf("%d",&n);
-    int array[n];
-    int *ptr=array;
-    int **ptr_array=&ptr;
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",**(ptr+i));
-    }
-    for(int i=0;i<n;i++)
-    {
-        printf(" %d",**(ptr+i));
-    }
+#include <stdio.h>
+
+int main() {
+  int n;
+
+  printf("Enter the number of elements: ");
+  scanf("%d", &n);
+
+  int array[n];
+  int *ptr = array;      // Single pointer to the array
+  int **ptr_array = &ptr; // Double pointer to point to the single pointer
+
+  printf("Enter %d numbers:\n", n);
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d", **ptr_array + i); // Access elements using pointer arithmetic
+  }
+
+  printf("The squares are: ");
+  for (int i = 0; i < n; i++)
+  {
+    printf(" %d",**(ptr_array+i));
+  }
+  printf("\n");
+
+  return 0;
 }
