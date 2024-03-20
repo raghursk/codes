a75c11721c11b1c05f8db00c871d2d99e7f0e5d6
raghursk <108519569+raghursk@users.noreply.github.com>
2024-03-20 11:26:02 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char string[10];
+    printf("entr the string:\n");
+    scanf("%s",string);
+    for(int i=0;string[i]!='\0';i++)
+    {
+        printf("%s",string);
+    }
+    char asc[256]={0};
+    printf("fiend the leragest elements in array is :\n");
+}
