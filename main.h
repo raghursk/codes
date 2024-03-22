bbf29f0eb0eca371296d5adbc0848586bbc9ccc3
raghursk <108519569+raghursk@users.noreply.github.com>
2024-03-22 19:30:26 +0530

    Add files via upload

---
+++ main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void fun(int n, int array[]);
+void reverse_string(const char *string);
+void find_middle(int n, int array[]);
+void custom_memcpy(char *dest, const char *src);
+void concatenate_strings();
